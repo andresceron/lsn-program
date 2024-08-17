e9f1fac85c47c6680336a917679c5849cd25f564
Andres Ceron <andres.ceron@burtcorp.com>
2024-08-17 22:50:50 +0200

    fixup! Add custom color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,33 +4,34 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    colors: {
-      extend: {},
-      transparent: 'transparent',
-      current: 'currentColor',
-      primary: {
-        lightest: '#3D458F',
-        light: '#2A3165',
-        DEFAULT: '#181b38',
-        dark: '#12152B',
-        darkest: '#0C0E1D',
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        primary: {
+          lightest: '#3D458F',
+          light: '#2A3165',
+          DEFAULT: '#181b38',
+          dark: '#12152B',
+          darkest: '#0C0E1D',
+        },
+        secondary: {
+          light: '#ffffff',
+          DEFAULT: '#e0f0fd',
+          dark: '#A0D0F8',
+        },
+        gray: {
+          // lightest: '#F7F6F3',
+          light: '#EEECE7',
+          DEFAULT: '#DFDAD0',
+          dark: '#BEB29D',
+          // darkest: '#928363'
+        },
+        white: {
+          DEFAULT: '#f8f8f8',
+        }
       },
-      secondary: {
-        light: '#ffffff',
-        DEFAULT: '#e0f0fd',
-        dark: '#A0D0F8',
-      },
-      gray: {
-        // lightest: '#F7F6F3',
-        light: '#EEECE7',
-        DEFAULT: '#DFDAD0',
-        dark: '#BEB29D',
-        // darkest: '#928363'
-      },
-      white: {
-        DEFAULT: '#f8f8f8',
-      }
-    },
+    }
   },
   plugins: [],
   prefix: 'tw-',
